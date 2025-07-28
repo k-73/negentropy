bb38806e6fff1394f318812eed1d09256256f5cb
kilo <k.machnowski@vertigo.global>
2025-07-28 23:30:55 +0200

    fix: Update ChangeListManager in workspace.xml to reference Block.cpp; rename label in Default.xml component to 'STM32'

--- Core/Diagram/Block.cpp
+++ Core/Diagram/Block.cpp
@@ -56,6 +56,16 @@ namespace Diagram {
         
         SDL_SetRenderDrawColor(renderer, borderR, borderG, borderB, borderA);
         SDL_RenderDrawRectF(renderer, &rect);
+
+        if (!data.label.empty()) {
+            ImDrawList* drawList = ImGui::GetBackgroundDrawList();
+            const ImVec2 textSize = ImGui::CalcTextSize(data.label.c_str());
+            const ImVec2 textPos(
+                screenPos.x + (rect.w - textSize.x) * 0.5f,
+                screenPos.y + (rect.h - textSize.y) * 0.5f
+            );
+            drawList->AddText(textPos, IM_COL32(255, 255, 255, 255), data.label.c_str());
+        }
     }
 
     void Block::xml_serialize(pugi::xml_node& node) const {
